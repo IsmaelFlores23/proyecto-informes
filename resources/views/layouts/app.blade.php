060200d4d70c6064c6d86aa82ccf14d57b37344a
Fabian Lanza <fabianlanza17@gmail.com>
2025-07-06 00:47:06 -0600

    Fix(dependencias) Se elimino dependencias duplicadas de Flowbite

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -101,11 +101,11 @@
     </script>
 
  <!-- asignar terna -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
+{{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script> --}}
     </body>
     
     <!-- form flotante-->
-    <script src="https://unpkg.com/flowbite@latest/dist/flowbite.js"></script>
+    {{-- <script src="https://unpkg.com/flowbite@latest/dist/flowbite.js"></script> --}}
 
 
 </html>
