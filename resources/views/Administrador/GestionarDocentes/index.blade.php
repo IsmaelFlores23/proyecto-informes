486f3f68a3e755f2d2b5ee59ea3131e7f538a344
Fabian Lanza <fabianlanza17@gmail.com>
2025-07-23 13:32:24 -0600

    Fondo negro en Modales Editar

--- resources/views/Administrador/GestionarDocentes/index.blade.php
+++ resources/views/Administrador/GestionarDocentes/index.blade.php
@@ -67,8 +67,7 @@
 
     <!-- Modal -->
     <div id="add-user-modal" tabindex="-1" aria-hidden="true" 
-         class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 
-                justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+         class="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-30 {{ isset($editando) && request()->is('GestionarDocentes/*/edit') ? '' : 'hidden' }}">
         <div class="relative p-4 w-full max-w-lg max-h-full">
             <div class="relative bg-white rounded-lg shadow">
                 <!-- Modal header -->
