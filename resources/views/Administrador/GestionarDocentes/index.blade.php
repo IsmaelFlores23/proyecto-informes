809c072e94efce624402864dded7e3452939c280
Alejandra-Dominguez <alejandradomin02@gmail.com>
2025-07-22 23:08:01 -0600

    Campo de telefono en los modales de Gestion de usurios

--- resources/views/Administrador/GestionarDocentes/index.blade.php
+++ resources/views/Administrador/GestionarDocentes/index.blade.php
@@ -21,6 +21,7 @@
                         <th class="px-6 py-3">N° Cuenta</th>
                         <th class="px-6 py-3">Nombre</th>
                         <th class="px-6 py-3">Email</th>
+                        <th class="px-6 py-3">Telefono</th>
                         {{-- <th class="px-6 py-3">Contraseña</th>
                         <th class="px-6 py-3">Rol</th> --}}
                         <th class="px-6 py-3">Facultad</th>
@@ -35,6 +36,7 @@
                             <td class="px-6 py-4">{{ $docente->numero_cuenta }}</td>
                             <td class="px-6 py-4 font-medium">{{ strtoupper($docente->name) }}</td>
                             <td class="px-6 py-4">{{ $docente->email }}</td>
+                            <td class="px-6 py-4">{{ $docente->telefono }}</td>
                             {{-- <td class="px-6 py-4">••••••••</td>  --}}
                             {{-- <td class="px-6 py-4">{{ $docente->role()->first()->nombre_role ?? 'Sin rol' }}</td> --}}
                             <td class="px-6 py-4">{{ $docente->facultad()->first()->nombre ?? 'Sin facultad' }}</td>
@@ -108,6 +110,12 @@
                                 <input name="email" placeholder="ejemplo@unicah.edu" type="email" 
                                      value="{{ old('email', $editando->email ?? '') }}"
                                      class="bg-gray-50 border border-gray-300 text-sm rounded-lg block w-full p-2.5" required>
+                            </div>
+                            <div>
+                                <label class="block mb-1 text-sm font-medium text-gray-900">Telefono</label>
+                                <input name="telefono" placeholder="####-####" type="text"
+                                    value="{{ old('telefono', $editando->telefono ?? '') }}"
+                                    class="bg-gray-50 border border-gray-300 text-sm rounded-lg block w-full p-2.5" required>
                             </div>
                             <div>
                                 <label class="block mb-1 text-sm font-medium text-gray-900">Contraseña</label>
