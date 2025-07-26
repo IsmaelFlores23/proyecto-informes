b26f72e5f2b72d727f278a3e07ba46a6376b29d8
Marthín José Contreras Rodríguez <Marthin J Contreras>
2025-07-25 21:11:57 -0600

    Correción de Traducción Ingles->Español en todo el Proyecto

--- resources/views/layouts/navigation-docente.blade.php
+++ resources/views/layouts/navigation-docente.blade.php
@@ -48,7 +48,7 @@
 
                     <x-slot name="content">
                         <x-dropdown-link :href="route('profile.edit')">
-                            {{ __('Profile') }}
+                            {{ __('Perfil Usuario') }}
                         </x-dropdown-link>
 
                         <!-- Authentication -->
@@ -58,7 +58,7 @@
                             <x-dropdown-link :href="route('logout')"
                                     onclick="event.preventDefault();
                                                 this.closest('form').submit();">
-                                {{ __('Log Out') }}
+                                {{ __('Cerrar Sesión') }}
                             </x-dropdown-link>
                         </form>
                     </x-slot>
@@ -100,7 +100,7 @@
 
             <div class="mt-3 space-y-1">
                 <x-responsive-nav-link :href="route('profile.edit')" :active="request()->routeIs('profile.edit')">
-                    {{ __('Profile') }}
+                    {{ __('Perfil Usuario') }}
                 </x-responsive-nav-link>
 
                 <!-- Authentication -->
@@ -110,7 +110,7 @@
                     <x-responsive-nav-link :href="route('logout')"
                             onclick="event.preventDefault();
                                         this.closest('form').submit();" :active="request()->routeIs('logout')">
-                        {{ __('Log Out') }}
+                        {{ __('Cerrar Sesión') }}
                     </x-responsive-nav-link>
                 </form>
             </div>
